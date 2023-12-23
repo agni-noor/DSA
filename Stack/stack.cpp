db206d78077829ef0e70b2821f083668ac131408
Agni Noor <agninoor04@gmail.com2>
2023-12-23 14:34:56 +0600

    isEmpty isFull Stack implementation

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 using namespace std;
 
+class Stack
+{
+
+private:
+    int top;
+    int arr[5];
+
+public:
+    Stack()
+    {
+        top = -1;
+        for (int i = 0; i < 5; i++)
+        {
+            arr[i] = 0;
+        }
+    }
+
+    bool isEmpty()
+    {
+        if (top == -1)
+            return true;
+        else
+            return false;
+    }
+
+    bool isFull()
+    {
+        if (top == 4)
+            return true;
+        else
+            return false;
+    }
+};
+
 int main()
 {
 }
